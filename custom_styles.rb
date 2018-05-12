2166f4687403d904f903b11704b474815c5e795e
Arthur Truong <arthurtruong.work@gmail.com>
2018-05-13 08:17:03 +1000

    first commit

---
+++ custom_styles.rb
@@ -0,0 +1,12 @@
+module GenApp
+  class Application < Rails::Application
+
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :erb
+      g.test_framework :test_unit, fixture: false
+      g.stylesheets false
+      g.javascripts
+
+  end
+end
